61694730c0f3fbebc28e82df629bb2795895c1ac
Luis Felipe <lfelipevasconcellos@hotmail.com>
2020-12-17 19:00:44 -0300

    display errors in input

---
+++ src/utils/getValidationErrors.ts
@@ -0,0 +1,16 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import { ValidationError } from 'yup';
+
+interface Errors {
+    [key: string]: string;
+}
+
+export default function getValidationErrors(err: ValidationError): Errors {
+    const validationErrors: Errors = {};
+
+    err.inner.forEach(error => {
+        validationErrors[error.path!] = error.message;
+    });
+
+    return validationErrors;
+}
