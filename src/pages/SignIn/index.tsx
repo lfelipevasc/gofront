91ad30ae6b269357b2cd6d2442a44a52412045d3
Luis Felipe <lfelipevasconcellos@hotmail.com>
2020-12-18 04:07:34 -0300

    toasts structure

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -4,7 +4,7 @@ import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
 
-import { useAuth } from '../../context/AuthContext';
+import { useAuth } from '../../hooks/AuthContext';
 import getValidationErrors from '../../utils/getValidationErrors';
 
 import logoImg from '../../assets/logo.svg';
@@ -50,6 +50,7 @@ const SignIn: React.FC = () => {
 
                     formRef.current?.setErrors(errors);
                 }
+                // disparar toast
             }
         },
         [signIn],
