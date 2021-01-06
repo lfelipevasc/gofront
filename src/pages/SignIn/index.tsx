fa1874a17727659b32aeb4b8112bc913ed65f94b
Luis Felipe <lfelipevasconcellos@hotmail.com>
2021-01-06 13:28:25 -0300

    creating first test

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -93,10 +93,10 @@ const SignIn: React.FC = () => {
 
                         <Button type="submit">Entrar</Button>
 
-                        <Link to="forgot-password">Esqueci minha senha</Link>
+                        <Link to="/forgot-password">Esqueci minha senha</Link>
                     </Form>
 
-                    <Link to="./signup">
+                    <Link to="/signup">
                         <FiLogIn />
                         Criar conta
                     </Link>
